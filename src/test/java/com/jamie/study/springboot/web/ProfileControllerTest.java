1da925cda1e9eaf8c73b8aef2a3e4a3958ea7c8c
noel <noelcoolblue@gmail.com>
2020-01-20 23:01:01 +0900

    -

--- src/test/java/com/jamie/study/springboot/web/ProfileControllerTest.java
+++ src/test/java/com/jamie/study/springboot/web/ProfileControllerTest.java
@@ -23,14 +23,11 @@ public class ProfileControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void profile은_인증없이_호출된다() throws Exception{
+    public void profile은_인증없이_호출된다() throws Exception {
         String expected = "default";
 
         ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
-
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-
         assertThat(response.getBody()).isEqualTo(expected);
-
     }
 }
